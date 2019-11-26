3eb5e8e06a802006e3b9f94cc925edd76c105172
david-amos <57191710+david-amos@users.noreply.github.com>
2019-11-26 08:24:56 -0600

    my first java commit

---
+++ Sammy/SammysMotto/src/SammysMotto.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class SammysMotto {
+    public static void main(String[] args){
+     JOptionPane.showMessageDialog(null, "Sammy's makes it fun in the sun", "Sammy's motto ",3);
+    }
+}
---
+++ Sammy/SammysMotto/src/SammysMottoWS.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class SammysMottoWS {
+    public static void main(String[] args){
+
+        System.out.println("ssssssssssssssssssssssssssssssssssssssssss");
+        System.out.println("s    sammy's makes it fun in the sun!    s");
+        System.out.println("ssssssssssssssssssssssssssssssssssssssssss");
+
+
+    }
+}
